f5da80756d5a02d2a0659a624b0aed835f9a0f01
CoinFLip2 <pierrelouis3131@gmail.com>
2023-07-26 11:59:29 +0200

    segement

--- idiot_solver.c
+++ idiot_solver.c
@@ -5,14 +5,14 @@
 int idiot_solver(sudoku *sud)
 {
     int running = 1;
-    int segment = 1;
+    int segments = 1;
     int x = 0;
     int y = 0;
     while (running)
     {
         running = 0;
-        segment = 0;
-        segment += segment(sud);
+        segments = 0;
+        segments += segment(sud);
         running += clib_nu(sud);
         running += clib_cache(sud);
         if (running != 0)
