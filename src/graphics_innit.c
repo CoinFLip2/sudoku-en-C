ba2c1d30a915034c2005ca6483a9ba2590e61584
CoinFLip2 <pierrelouis3131@gmail.com>
2023-07-27 12:13:18 +0200

    7

---
+++ src/graphics_innit.c
@@ -0,0 +1,25 @@
+#include"fonctions.h"
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL.h>
+
+
+void innitGraphics(sudoku *sud)
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
+    sud -> window = SDL_CreateWindow("Sudoku", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    sud -> rederer = SDL_CreatrRenderer(sud -> window, -1, SDL_RENDERER_ACCELERATED);
+    init_grid_surface(sud);
+    init_font(sud);
+}    
+
+void quitGraphics(sudoku *sud)
+{
+    SDL_DestroyRenderer(sud -> renderer);
+    SDL_DestroyWindow(sud -> window);
+    SDL_DestroyTexture(sud -> gridTexture);
+    TTF_Quit();
+    SDL_Quit();
+    for (int i = 0; i < 9; i++)
+        SDL_DestroyTexture(sud -> cellTexture[i])
+}
